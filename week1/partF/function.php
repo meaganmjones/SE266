87627b3f1d22ec3358319ee7254369e1893ba9a7
Meagan Jones <mmjones@email.neit.edu>
2022-03-30 17:09:23 -0400

    finish partF

--- week1/partF/function.php
+++ week1/partF/function.php
@@ -4,9 +4,14 @@
 $games = ['Minecraft', 'Tetris', 'Pokemon'];
 
 //die and dump function
+//dumps string info out and dies
 function dd($temp){
-    var_dump($temp);
+    echo '<pre>';
+    die(var_dump($temp));
+    echo '</pre>';
 }
 
+
+//call dd function for games array
 dd($games);
 
