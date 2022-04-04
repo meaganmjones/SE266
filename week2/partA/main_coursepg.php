b5d027163c025a3c6f3a815c4d1a82a9eeec1030
meaganmjones <mmjones@email.neit.edu>
2022-04-04 09:37:42 -0400

    creating main page

---
+++ week2/partA/main_coursepg.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css"/>
+    <title>PHP Course</title>
+</head>
+<body>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="main_coursepg.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="https://github.com/meaganmjones/SE266">GitHub Repo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Learn Git</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Meagan's Hobbies</a>
+  </li>
+
+</ul>
+    
+    <h1>Meagan Jones' PHP Course</h1>
+
+    <h2>Learn GitHub: </h2>
+        <li>https://lab.github.com/githubtraining/introduction-to-github</li>
+        <li>https://www.youtube.com/watch?v=nhNq2kIvi9s</li>
+        <li>https://www.codecademy.com/learn/learn-git</li>
+
+    <h2>Meagan's Hobbies</h2>
+        <li>Paintball https://www.nxlpaintball.com/</li>
+        <li>PC Building https://www.corsair.com/us/en/build-your-gaming-pc?gclid=CjwKCAjwrqqSBhBbEiwAlQeqGj-jhO1ZX_XxjpnVJOyq0l2j0qICpCfKDQs7yaSHnR2Fpvg4kH__hRoCs9IQAvD_BwE</li>
+        <li></li>
+
+    <h2>PHP Assignments</h2>
+        <li>Week 1 - <a href="se266/se266-1/week1/partG/fizzbuzz.php">FizzBuzz</a></li>
+        <li>Week 2</li>
+        <li>Week 3</li>
+        <li>Week 4</li>
+        <li>Week 5</li>
+        <li>Week 6</li>
+        <li>Week 7</li>
+        <li>Week 8</li>
+        <li>Week 9</li>
+        <li>Week 10</li>
+
+
+
+
+
+    <?php include __DIR__ . '/../include/footer.php'; ?>
+</body>
+</html>
