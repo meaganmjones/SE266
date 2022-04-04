0c18a4e6619538f16fc107ccdc9ef1bc3086d426
meaganmjones <mmjones@email.neit.edu>
2022-04-04 15:31:13 -0400

    start of intake form

---
+++ week2/partB/intake_form.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthy Living Intake</title>
+</head>
+<body>
+    <h1>ADD THE NAV PLS</h1>
+
+    <h1>Patient Intake</h1>
+    <form name="patient_form" method="post" action="intake_form.php">
+
+        <div class="container">
+            <div class="label">
+                <label>First Name:</label>
+                <input type="text" name="fName" value="" />
+            </div>
+            <div>
+                <label>Last Name:</label>
+                <input type="text" name="lName" value="" />
+            </div>
+            <div>
+                <label>Marital Status:</label>
+                <input type="radio" name="status" value="Not Married"/>Not Married
+                <input type="radio" name="status" value="Married"/>Married
+            </div>
+            <div>
+                <label>Date of Birth:</label>
+                <input type="date" name="dob" value="" />
+            </div>
+            <div>
+                <label>Height:</label>
+                <input type="text" name="feet" value="" />
+                <input type="text" name="inches" value="" />
+            </div>
+            <div>
+                <label>Weight:</label>
+                <input type="text" name="weight" value="" />
+            </div>
+
+
+</body>
+</html>
