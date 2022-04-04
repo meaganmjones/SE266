41476ab3747afa286f3b9a2429d726296ac6e199
meaganmjones <mmjones@email.neit.edu>
2022-04-04 09:45:28 -0400

    created pages for nav links

--- week2/partA/main_coursepg.php
+++ week2/partA/main_coursepg.php
@@ -10,34 +10,25 @@
 <body>
 <ul class="nav">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
     <a class="nav-link" href="main_coursepg.php">Home</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="https://github.com/meaganmjones/SE266">GitHub Repo</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Learn Git</a>
+    <a class="nav-link" href="learnGit.php">Learn Git</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Meagan's Hobbies</a>
+    <a class="nav-link" href="hobbies.php">Meagan's Hobbies</a>
   </li>
 
 </ul>
     
     <h1>Meagan Jones' PHP Course</h1>
 
-    <h2>Learn GitHub: </h2>
-        <li>https://lab.github.com/githubtraining/introduction-to-github</li>
-        <li>https://www.youtube.com/watch?v=nhNq2kIvi9s</li>
-        <li>https://www.codecademy.com/learn/learn-git</li>
 
-    <h2>Meagan's Hobbies</h2>
-        <li>Paintball https://www.nxlpaintball.com/</li>
-        <li>PC Building https://www.corsair.com/us/en/build-your-gaming-pc?gclid=CjwKCAjwrqqSBhBbEiwAlQeqGj-jhO1ZX_XxjpnVJOyq0l2j0qICpCfKDQs7yaSHnR2Fpvg4kH__hRoCs9IQAvD_BwE</li>
-        <li></li>
+
+
 
     <h2>PHP Assignments</h2>
         <li>Week 1 - <a href="se266/se266-1/week1/partG/fizzbuzz.php">FizzBuzz</a></li>
