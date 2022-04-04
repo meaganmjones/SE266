28b1d55a4f8ff3c1246307a2b371816babf87094
meaganmjones <mmjones@email.neit.edu>
2022-04-04 09:45:46 -0400

    adding pages

---
+++ week2/partA/hobbies.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css"/>
+    <title>Learn Git</title>
+</head>
+<body>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link" href="main_coursepg.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="https://github.com/meaganmjones/SE266">GitHub Repo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="learnGit.php">Learn Git</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Meagan's Hobbies</a>
+  </li>
+
+</ul>
+
+<h2>Meagan's Hobbies</h2>
+        <li><a href="https://www.nxlpaintball.com/">Paintball</a></li>
+        <li><a href="https://www.corsair.com/us/en/build-your-gaming-pc?gclid=CjwKCAjwrqqSBhBbEiwAlQeqGj-jhO1ZX_XxjpnVJOyq0l2j0qICpCfKDQs7yaSHnR2Fpvg4kH__hRoCs9IQAvD_BwE">PC Building</a></li>
+        <li></li>
+</body>
+</html>
---
+++ week2/partA/learnGit.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css"/>
+    <title>Learn Git</title>
+</head>
+<body>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link" href="main_coursepg.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="https://github.com/meaganmjones/SE266">GitHub Repo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="learnGit.php">Learn Git</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Meagan's Hobbies</a>
+  </li>
+
+</ul>
+<h2>Learn GitHub: </h2>
+        <li><a href="https://lab.github.com/githubtraining/introduction-to-github">Github Training</a></li>
+        <li><a href="https://www.youtube.com/watch?v=nhNq2kIvi9s">YouTube Tutorial</a></li>
+        <li><a href="https://www.codecademy.com/learn/learn-git">Code Acedemy</a></li>
+</body>
+</html>
