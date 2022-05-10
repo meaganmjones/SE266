6998f0c739eb2053ff2fe23d511dc7188d90d814
Meagan Jones <mmjones@email.neit.edu>
2022-05-10 14:31:38 -0400

    not working yet

--- week6/schools/schoolUpload.php
+++ week6/schools/schoolUpload.php
@@ -1,5 +1,5 @@
 <?php
-   include_once __DIR__ . "/model/Schools.php";
+   include_once __DIR__ . "/model/schools.php";
    include_once __DIR__ . "/include/function.php";
       
 
@@ -18,21 +18,22 @@
         //      *** Make sure that the upload directory is writeable!
 
         $temp_name = $_FILES['fileToUpload']['tmp_name']; 
-        $path = getcwd(). DIRECTORY_SEPARATOR. 'upload';
+        $path = getcwd(). DIRECTORY_SEPARATOR. UPLOAD_DIRECTORY;
         $target_file = $path . DIRECTORY_SEPARATOR . $_FILES['fileToUpload']['name'];
 
-        $file = move_uploaded_file($temp_name, $target_file);
+        move_uploaded_file($temp_name, $target_file);
         //echo 'success';
 
+        $insertSchool = $schoolDatabase->insertSchoolsFromFile($target_file);
 
-        if(!file_exists("upload/schools.csv")){
-            echo 'File does not exist';
-            //exit;
-        }else{
-            //$insertFile = fopen('upload/schools.csv', 'rb');
-            $insertSchool = $schoolDatabase->insertSchoolsFromFile($target_file);
-            var_dump($insertSchool);
-        }
+        // if(!file_exists("upload/schools.csv")){
+        //     echo 'File does not exist';
+        //     //exit;
+        // }else{
+        //     //$insertFile = fopen('upload/schools.csv', 'rb');
+        //     $insertSchool = $schoolDatabase->insertSchoolsFromFile($target_file);
+        //     var_dump($insertSchool);
+        // }
 
     }
 
