bdff370a46f12f0aaf02c72592dfe140023f6ccf
Meagan Jones <mmjones@email.neit.edu>
2022-05-11 20:19:49 -0400

    init

---
+++ week7/disneyVote.php
@@ -0,0 +1,42 @@
+<?php
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel='stylesheet' href='vote.css' />
+</head>
+<body>
+        <div class='main'>
+            <div class='char'>
+                <h3>Cinderella</h3>
+                <img class='img' src=#>
+                <button name='cinderella' type='submit'>Vote for Cinderella</button>
+                <div>
+                    <h4 id='cinderella-votes'><?php ?></h4>
+                </div>
+            </div>
+            <div class='char'>
+                <h3>Mulan</h3>
+                <img class='img' src=#>
+                <button name='mulan' type='submit'>Vote for Mulan</button>
+            </div>
+            <div class='char'>
+                <h3>Snow White</h3>
+                <img class='img' src=#>
+                <button name='snow' type='submit'>Vote for Snow White</button>
+            </div>
+        </div>
+    </form>
+    
+</body>
+</html>
